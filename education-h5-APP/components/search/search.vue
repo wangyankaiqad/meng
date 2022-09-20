964b52af9af0f26fd6f0e6828850610287b823a7
wangyankaiqad <2857016372@qq.com>
2022-09-20 17:37:35 +0800

    '9-20号'

---
+++ education-h5-APP/components/search/search.vue
@@ -0,0 +1,35 @@
+<template>
+	<navigator url="/pages/searchyemian/searcgyemian">
+		<view><input type="text" placeholder="请输入搜索到额内容" class="input" disabled @click="add" /></view>
+	</navigator>
+</template>
+
+<script>
+export default {
+	name: 'search',
+	setup() {
+		const add=()=>{
+			console.log(123);
+			
+		}
+		return{
+			add
+		}
+	}
+
+};
+</script>
+
+<style>
+.input {
+	border-radius: 50px;
+	width: 95%;
+	outline: none;
+	text-align: center;
+	margin: 10px;
+	height: 30px;
+	background-color: #fff;
+	position: fixed;
+	z-index: 999;
+}
+</style>
---
+++ education-h5-APP/pages/searchyemian/searcgyemian.vue
@@ -0,0 +1,11 @@
+<template>
+	<view class="a1">
+		搜索页面
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
